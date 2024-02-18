f07ae07f2daf3c942422d803ea6a29e9c38b77e8
Jeferson Barros <im.jbalves@gmail.com>
2024-02-18 17:23:36 -0300

    commit project to run at aws ec2

---
+++ src/main/java/br/com/jeffcreativesolutions/desafiotodolist/controller/TodoController.java
@@ -0,0 +1,39 @@
+package br.com.jeffcreativesolutions.desafiotodolist.controller;
+
+import br.com.jeffcreativesolutions.desafiotodolist.entity.Todo;
+import br.com.jeffcreativesolutions.desafiotodolist.service.TodoService;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    List<Todo> create(@RequestBody @Valid Todo todo){
+        return todoService.create(todo);
+    }
+
+    @GetMapping
+    List<Todo> list() {
+        return todoService.list();
+    }
+
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo) {
+        return todoService.update(todo);
+    }
+
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id) {
+        return todoService.delete(id);
+    }
+}
